6ae8d1e86dad41a82b93d667308f33e94af61f96
buteanbogdan <b.bogdan2302@gmail.com>
2026-03-16 21:44:07 +0200

    Add eager singleton

---
+++ Singleton/src/ManagementClinica.java
@@ -0,0 +1,50 @@
+public class ManagementClinica {
+    private String numeClinica;
+    private String adresa;
+    private int numarDoctori;
+
+    // Instanta este creata imediat (Eager)
+    private static final ManagementClinica instanta = new ManagementClinica();
+
+    // Constructor privat
+    private ManagementClinica() {
+        this.numeClinica = "MedLife Premium";
+        this.adresa = "Strada Victoriei, Nr. 10";
+        this.numarDoctori = 12;
+    }
+
+    // Metoda publica prin care oferim acces la instanța unica
+    public static ManagementClinica getInstanta() {
+        return instanta;
+    }
+
+    // Getters si Setters
+    public String getNumeClinica() {
+        return numeClinica;
+    }
+
+    public void setNumeClinica(String numeClinica) {
+        this.numeClinica = numeClinica;
+    }
+
+    public String getAdresa() {
+        return adresa;
+    }
+
+    public void setAdresa(String adresa) {
+        this.adresa = adresa;
+    }
+
+    public int getNumarDoctori() {
+        return numarDoctori;
+    }
+
+    public void setNumarDoctori(int numarDoctori) {
+        this.numarDoctori = numarDoctori;
+    }
+
+    @Override
+    public String toString() {
+        return "Clinica: " + numeClinica + ", Adresa: " + adresa + ", Doctori: " + numarDoctori;
+    }
+}
