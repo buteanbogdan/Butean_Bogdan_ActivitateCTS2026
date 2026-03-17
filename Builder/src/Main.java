90f5987492787c5219c56e6bc1e77fc8e74710ef
buteanbogdan <b.bogdan2302@gmail.com>
2026-03-17 20:58:15 +0200

    testare builder main

--- Builder/src/Main.java
+++ Builder/src/Main.java
@@ -1,5 +1,31 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("--- Testare Builder Rezervare Restaurant ---");
+        RezervareRestaurant rezervareCuplu = new RezervareRestaurantBuilder()
+                .setAsezareLaGeam(true)
+                .setDecoratieRomantica(true)
+                .setMuzicaLive(true)
+                .build();
+
+        System.out.println("Rezervare 1 (Cuplu):");
+        System.out.println(rezervareCuplu);
+        System.out.println("--------------------------------------------------");
+
+        RezervareRestaurant rezervareFamilie = new RezervareRestaurantBuilder()
+                .setScaunCopil(true)
+                .setMeniuVegan(true)
+                .build();
+
+        System.out.println("Rezervare 2 (Familie):");
+        System.out.println(rezervareFamilie);
+        System.out.println("--------------------------------------------------");
+
+        RezervareRestaurant rezervareSimpla = new RezervareRestaurantBuilder()
+                .build();
+
+        System.out.println("Rezervare 3 (Standard):");
+        System.out.println(rezervareSimpla);
     }
 }
